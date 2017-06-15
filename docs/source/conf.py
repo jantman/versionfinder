dc958150eb4be5b52257d4d9710595fd62f24e21
Jason Antman <jason@jasonantman.com>
2017-06-15 18:30:00 -0400

    use py3 docs for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -293,7 +293,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/2/': None,
+    'https://docs.python.org/3/': None,
 }
 
 autoclass_content = 'class'
